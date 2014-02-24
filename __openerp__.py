04a6f186e6b7bb33141567309aff06eb0d05437e
Jesse Vartiainen <jezco@Jesses-MacBook-Pro.local>
2014-02-24 13:33:38 +0200

    initial commit

---
+++ __openerp__.py
@@ -0,0 +1,17 @@
+{
+    "name" : "Verts Unique Product Code ",
+    "version" : "1.0",
+    "author" : "Verts Team",
+    "description" : """This module will make default code of product unique""",
+    "website"  : "http://www.verts.co.in",
+    "depends"  : ["base","product","stock","purchase"],
+    "category" : "Generic Modules",
+    "init_xml" : [],
+    "demo_xml" : [],
+    "data"     : [ 
+                 ],
+    'test': [],
+    'installable': True,
+    'active': False,
+    'certificate': '',
+}
---
+++ product.py
@@ -0,0 +1,31 @@
+from openerp.osv import osv, fields
+import netsvc
+import decimal_precision as dp
+# import openerp.addons.decimal_precision as dp
+import time
+from tools.translate import _
+
+class product_product(osv.osv):
+    _name="product.product"
+    _inherit='product.product'
+    
+ 
+    
+    def create(self, cr, uid, vals, context=None):
+        obj_id = super(product_product, self).create(cr, uid, vals, context=context)
+        for val in self.browse(cr,uid,[obj_id]):
+            product_id=self.search(cr,uid,[('default_code','=',val.default_code.lower()),('id','!=',val.id)])
+            if len(product_id)>0:
+                raise osv.except_osv(('Warning'),("Please choose different code"))        
+        #self.create_send_note(cr, uid, [obj_id], context=context)
+        return obj_id
+    
+    
+    def write(self, cr, uid, ids,vals, context=None):
+        obj_id = super(product_product, self).write(cr, uid,ids, vals, context=context)
+        for val in self.browse(cr,uid,ids):
+            product_id=self.search(cr,uid,[('default_code','=',val.default_code.lower()),('id','!=',val.id)])
+            if len(product_id)>0:
+                raise osv.except_osv(('Warning'),("Please choose different code"))        
+        #self.create_send_note(cr, uid, [obj_id], context=context)
+        return obj_id
